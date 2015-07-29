0e9aeb0dd045c5bf125d1ec3283970f33ddd7fa8
Jetsonhacks <jim@jetsonhacks.com>
2015-07-29 10:47:45 -0700

    Script cleanup

--- installROSPocketSphinx.sh
+++ installROSPocketSphinx.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-sudo apt-get install gstreamer0.10-pocketsphinx -y
-sudo apt-get install gstreamer0.10-gconf -y
-sudo apt-get install python-gst0.10 -y
+# Add the gstreamer dependencies
+sudo apt-get install gstreamer0.10-pocketsphinx gstreamer0.10-gconf python-gst0.10 -y
 # Assume that the installation for the JetsonBot is in ~/jetsonbot
 cd ~/jetsonbot/src
 git clone https://github.com/jetsonhacks/pocketsphinx.git
