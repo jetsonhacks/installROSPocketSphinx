8c75c7a062449be3fd3878f0422eb40267c228b5
Jetsonhacks <jim@jetsonhacks.com>
2015-07-29 11:07:44 -0700

    Add xvfb

--- installROSPocketSphinx.sh
+++ installROSPocketSphinx.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Add the gstreamer dependencies
 sudo apt-get install gstreamer0.10-pocketsphinx gstreamer0.10-gconf python-gst0.10 -y
+# Install virtual x11 for running pocketsphinx on JetsonBot without attached display
+sudo apt-get install xvfb -y
 # Assume that the installation for the JetsonBot is in ~/jetsonbot
 cd ~/jetsonbot/src
 git clone https://github.com/jetsonhacks/pocketsphinx.git
